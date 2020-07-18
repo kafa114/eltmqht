692c07492c5471ee9d3b3e15027a7b115933cc2f
djs226587 <66864385+djs226587@users.noreply.github.com>
2020-07-18 23:42:57 +0900

    Update eltmqht.py

--- eltmqht.py
+++ eltmqht.py
@@ -13,7 +13,7 @@ async def on_ready():
     while True:
         user = len(client.users)
         server = len(client.guilds)
-        messages = [str(user) + "명의 유저들과 함께 하는중입니다.", str(server) + "개의 서버에 참가되어있습니다.", "봇이 오프라인이라면 점검중입니다.", "공식서버에 참가하여 빠르게 소식을 들어보세요.", "Ds도움말 / Dshelp"]
+        messages = [str(user) + "명의 유저들과 함께 하는중입니다.", str(server) + "개의 서버에 참가되어있습니다.", "봇이 오프라인이라면 점검중입니다.", "공식서버에 참가하여 빠르게 소식을 들어보세요.", "Dshelp / Ds도움말"]
         for (m) in range(5):
             await client.change_presence(status=discord.Status.online, activity=discord.Activity(name=messages[(m)], type=discord.ActivityType.watching))
             await asyncio.sleep(10)
@@ -31,11 +31,11 @@ async def on_message(message):
     if message.author.bot:
         return None
 
-    if message.content == "Ds도움말" or message.content == "Dshelp":
-        Dshelp = discord.Embed(title="**디스봇 / 도움말**", color=0x8affc6)
-        Dshelp.add_field(name="1", value="2", inline=False)
+    if message.content == "Dshelp" or message.content == "Ds도움말":
+        Dshelp = discord.Embed(title="**[ 디스봇 ] 도움말**", color=0x8affc6)
+        Dshelp.add_field(name="기본명령어", value="- Ds( 명령어 )\n- Dshelp / Ds도움말 : 디스봇 도움말을 보여줍니다.", inline=False)
         Dshelp.set_thumbnail(url="https://media.discordapp.net/attachments/734054856678965292/734055038607163412/d2c8518820b21bd3.png")
-        Dshelp.set_footer(text="3" , icon_url="https://media.discordapp.net/attachments/734054856678965292/734055038607163412/d2c8518820b21bd3.png")
+        Dshelp.set_footer(text="DissBot" , icon_url="https://media.discordapp.net/attachments/734054856678965292/734055038607163412/d2c8518820b21bd3.png")
         await message.channel.send(embed=Dshelp)
 
 access_token = os.environ["token"]
